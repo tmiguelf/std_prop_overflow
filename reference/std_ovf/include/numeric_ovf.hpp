6c8158869417b1650badeb7b2c7a6b33f8dd582b
Tiago Freire <tmiguelf@hotmail.com>
2025-02-11 23:47:07 +0100

    inline corrections

--- reference/std_ovf/include/numeric_ovf.hpp
+++ reference/std_ovf/include/numeric_ovf.hpp
@@ -17,12 +17,12 @@
 
 #endif
 
-#if ( defined (__GNUG__) || defined(__GNUC__) )
-#	define FORCE_INLINE __attribute__((always_inline))
-#elif ( defined (_MSC_VER ) )
+#if (defined(__GNUG__) || defined(__GNUC__))
+#	define FORCE_INLINE __attribute__((always_inline)) inline
+#elif (defined(_MSC_VER))
 #	define FORCE_INLINE __forceinline
 #else
-#	define FORCE_INLINE
+#	define FORCE_INLINE inline
 #endif
 
 //__readeflags
